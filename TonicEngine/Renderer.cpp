96010ec068963b6e458dde83948a5dad50c5cdb4
jarnepeire <jarne.peire@gmail.com>
2021-02-21 21:52:32 +0100

    Implemented ImGui + Start Buttons
    Implemented the ImGui library and linked everything to the engine. A few start buttons are being displayed to start with.

--- TonicEngine/Renderer.cpp
+++ TonicEngine/Renderer.cpp
@@ -3,7 +3,11 @@
 #include <SDL.h>
 #include "SceneManager.h"
 #include "Texture2D.h"
+#include "imgui.h"
+#include "backends/imgui_impl_opengl2.h"
+#include "backends/imgui_impl_sdl.h"
 
+using namespace dae;
 int GetOpenGLDriverIndex()
 {
 	auto openglIndex = -1;
@@ -20,11 +24,18 @@ int GetOpenGLDriverIndex()
 
 void dae::Renderer::Init(SDL_Window * window)
 {
+	m_pWindow = window;
 	m_Renderer = SDL_CreateRenderer(window, GetOpenGLDriverIndex(), SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (m_Renderer == nullptr) 
 	{
 		throw std::runtime_error(std::string("SDL_CreateRenderer Error: ") + SDL_GetError());
 	}
+
+	//ImGui
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplSDL2_InitForOpenGL(window, SDL_GL_GetCurrentContext());
+	ImGui_ImplOpenGL2_Init();
 }
 
 void dae::Renderer::Render() const
@@ -32,12 +43,25 @@ void dae::Renderer::Render() const
 	SDL_RenderClear(m_Renderer);
 
 	SceneManager::GetInstance().Render();
+
+	ImGui_ImplOpenGL2_NewFrame();
+	ImGui_ImplSDL2_NewFrame(m_pWindow);
+	ImGui::NewFrame();
+
+	(m_ShowDemoWindow) ? ImGui::ShowDemoWindow() : DisplayUI();
+
+	ImGui::Render();
+	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 	
 	SDL_RenderPresent(m_Renderer);
 }
 
 void dae::Renderer::Destroy()
 {
+	ImGui_ImplOpenGL2_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+
 	if (m_Renderer != nullptr)
 	{
 		SDL_DestroyRenderer(m_Renderer);
@@ -62,4 +86,19 @@ void dae::Renderer::RenderTexture(const Texture2D& texture, const float x, const
 	dst.w = static_cast<int>(width);
 	dst.h = static_cast<int>(height);
 	SDL_RenderCopy(GetSDLRenderer(), texture.GetSDLTexture(), nullptr, &dst);
+}
+
+void dae::Renderer::DisplayUI() const
+{
+	ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(0.59f, 0.52f, 1.f));
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(0.60f, 0.56f, 1.f));
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(0.61f, 0.60f, 1.f));
+	ImGui::Button("Single Player");
+
+	ImGui::SameLine();
+	ImGui::Button("Co-Op");
+
+	ImGui::SameLine();
+	ImGui::Button("Versus");
+	ImGui::PopStyleColor(3);
 }
--- TonicEngine/Renderer.h
+++ TonicEngine/Renderer.h
@@ -21,8 +21,13 @@ namespace dae
 		void RenderTexture(const Texture2D& texture, float x, float y, float width, float height) const;
 
 		SDL_Renderer* GetSDLRenderer() const { return m_Renderer; }
+		SDL_Window* GetSDLWindow() const { return m_pWindow; }
 	private:
+		SDL_Window* m_pWindow{};
 		SDL_Renderer* m_Renderer{};
+
+		bool m_ShowDemoWindow{ false };
+		void DisplayUI() const;
 	};
 }
 
